aa78decc43862b42cc2bcae44602fba1b1b9fc2b
Oleksii Maiboroda <alexnik0888@gmail.com>
2017-11-29 15:28:29 +0200

    example

--- app/src/main/java/com/maiboroda/o/example/ExampleActivity.kt
+++ app/src/main/java/com/maiboroda/o/example/ExampleActivity.kt
@@ -1,15 +1,16 @@
-package com.maiboroda.o.sectioncirclebar
+package com.maiboroda.o.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.maiboroda.o.example.R.*
 import kotlinx.android.synthetic.main.activity_example.*
 
 class ExampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_example)
+        setContentView(layout.activity_example)
 
         wheel.setListener { section -> Toast.makeText(this, section.toString(), Toast.LENGTH_SHORT).show() }
     }
